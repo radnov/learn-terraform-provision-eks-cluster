04c880f7575ddc46abc820f29b95d872d4c2b9ca
radnov <radnov@pm.me>
2021-05-17 11:42:52 +0300

    Add explanation to commented out code

--- kubernetes.tf
+++ kubernetes.tf
@@ -12,7 +12,7 @@ provider "kubernetes" {
   */
   token = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  /*
+  /* This "old" way of authentication works with a named profile
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command = "aws"
